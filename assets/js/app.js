e3e8e90fe575296521f775c089d2beeb980fbea5
kazitanviranjum <96996966+kazitanviranjum@users.noreply.github.com>
2022-03-12 04:25:28 +0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.banner_slider').slick({
+        prevArrow: '<i class="fas fa-arrow-left banner_slider_icon"></i>',
+        nextArrow: '<i class="fas fa-arrow-right banner_slider_icon"></i>'
+    })
+})
